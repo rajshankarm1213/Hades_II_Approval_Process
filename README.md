dd9c646d6591d9e0adccc23bd5ab83b2a3a9e6b4
M Rajshankar <rajshankarm1729@gmail.com>
2024-05-27 18:05:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Hades II Approval Process
+
+Mod for the game [Hades II](https://store.steampowered.com/app/1145350/Hades_II/) that adds an additional "Oath of the Unseen" Fear option: _Approval Process_, from the original game [Hades](https://store.steampowered.com/app/1145360/Hades/).
+
+## Features
+
+- Adds additional Fear options to the "Oath of the Unseen" bounties, in the form of _Approval Process_ from the "Pact of Punishment" bounty in the original game.
+- **Approval Process**:
+  - You have fewer choices when offered Boons, items, or upgrades, 1 per rank.
+- Two ranks in total, the first one adds 2 Fear and the second one adds 3 Fear. This takes the maximum possible Fear count to 60. (As of v0.91027)
+
+## Installation
+
+- Installation Instructions coming soon
+
+## Releases
+
+- Releases coming soon
---
+++ src/ready.lua
@@ -0,0 +1,42 @@
+---@meta _
+-- globals we define are private to our plugin!
+---@diagnostic disable: lowercase-global
+
+-- here is where your mod sets up all the things it will do.
+-- this file will not be reloaded if it changes during gameplay
+-- 	so you will most likely want to have it reference
+--	values and functions later defined in `reload.lua`.
+
+local upgradeName = 'ReducedLootChoicesShrineUpgrade'
+local metaUpgradesTable = game.MetaUpgradeData
+local shrineUpgradesTable = game.ShrineUpgradeOrder
+local traitText = rom.path.combine(rom.paths.Content, 'Game/Text/en/TraitText.en.sjson')
+--local GameState_shrineUpgrades = game.ShrineUpgrades
+metaUpgradesTable.ReducedLootChoicesShrineUpgrade =
+{
+	InheritFrom = { "BaseMetaUpgrade", },
+	Icon = "ShrineIcon_LockedChoice",
+	InactiveChangeValue = 0,
+	Ranks = {
+		{ Points = 2, ChangeValue = 1 },
+		{ Points = 3, ChangeValue = 2 },
+	},
+	Name = "ReducedLootChoicesShrineUpgrade",
+	ShortTotal = "ReducedLootChoicesShrineUpgrade_ShortTotal",
+}
+
+game.ShrineUpgrades = {
+	ReducedLootChoicesShrineUpgrade = 0, -- AP value is 0 by default
+}
+
+
+table.insert(shrineUpgradesTable, upgradeName)
+
+sjson.hook(traitText, function(sjsonData)
+	local order = {"Id", "DisplayName", "Description"}
+	table.insert(sjsonData.Texts, sjson.to_object({
+			Id = "ReducedLootChoicesShrineUpgrade",
+			DisplayName = "Vow of Acceptance",
+			Description = "You have {#ShrinePenaltyFormat} {$MetaUpgradeData.ReducedLootChoicesShrineUpgrade.ChangeValue} {#Prev} fewer choice(s) when offered {$Keywords.GodBoonPlural}, items, or upgrades."
+	}, order))
+end)
